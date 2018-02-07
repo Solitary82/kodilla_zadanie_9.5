ef1a344c01cb98c63fc049ce82b0558d87afd314
Marta Waplak <mwaplak@gmail.com>
2018-02-07 19:54:25 +0100

    Fix .js file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var newElements = document.getElementsByClassName("button");
 var elementNumber = newElements.length;
 
-for (var i = 0; i <= elementNumber; i++) {
+for (var i = 0; i < elementNumber; i++) {
     var contents = newElements[i].innerText;
     alert(contents);
 }
